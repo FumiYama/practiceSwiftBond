6513c48fa602c25a825a342358196d82e925a0b3
FumiYama <f.yamanaka1127@gmail.com>
2016-06-26 16:43:19 +0900

    Initial Commit

---
+++ practiceSwiftBond/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practiceSwiftBond
+//
+//  Created by Fumiya Yamanaka on 2016/06/26.
+//  Copyright © 2016年 yamanaka fumiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
